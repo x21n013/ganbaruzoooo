21e4a2062880961bb8c1197a3a4648e453e965c1
x21n052 <x21n052@mitsuhashi.ac.jp>
2022-09-27 13:24:17 +0900

    add

---
+++ ganbaruzoooo/Assets/HPs/IAttackDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Bonfire.Combat
+{
+    //IEventSystemHandlerを継承させる
+    public interface IAttackDamage : IEventSystemHandler
+    {
+        void OnDamaged(AttackInfo attackInfo);
+    }
+}
