4ef5338cd44bd797ddb3830b8c61899a8a60c095
x21n005 <103475890+x21n005@users.noreply.github.com>
2022-10-17 12:34:54 +0900

    add file

--- ganbaruzoooo/Assets/filed/Main 和泉 1/WarpStart.cs
+++
@@ -1,6 +0,0 @@
-void Start () {
-	characterController = GetComponent<CharacterController> ();
-	animator = GetComponent <Animator> ();
-	state = WarpCharaState.normal;
-	instantiateParticle = GetComponent<InstantiateParticle> ();
-}
