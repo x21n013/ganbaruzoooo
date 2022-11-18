0353d1f46df0352033105c282d458b5857dcd22a
x21n036 <x21n036@mitsuhashi.ac.jp>
2022-11-18 14:46:56 +0900

    1118

--- ganbaruzoooo/Assets/Scripts/PlayerManager.cs
+++ ganbaruzoooo/Assets/Scripts/PlayerManager.cs
@@ -14,6 +14,10 @@ public class PlayerManager : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+
+        /*PlayerPrefs.GetFloat("zahyoux",0);
+        PlayerPrefs.GetFloat("zahyouy",0);
+        PlayerPrefs.GetFloat("zahyouz",0);*/
     }
 
     // Update is called once per frame
@@ -46,4 +50,21 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
+    //セーブ
+    /*private void Save()
+    {
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+        float positionX = transform.potion.x;
+        float positionY = transform.potion.y;
+        float positionZ = transform.potion.z;
+
+        PlayerPrefs.SetFloat("zahyoux",positionX);
+        PlayerPrefs.SetFloat("zahyouy",positionY);
+        PlayerPrefs.SetFloat("zahyouz",positionZ);
+
+        PlayerPrefs.Save();
+        }
+    }*/
+    
 }
